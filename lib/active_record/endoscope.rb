a039a29c122d26e1235946c39316787972079467
Toru KAWAMURA <tkawa@4bit.net>
2013-09-11 17:51:28 +0900

    Implement ActiveRecord::Endoscope#scope

--- lib/active_record/endoscope.rb
+++ lib/active_record/endoscope.rb
@@ -1,7 +1,16 @@
-require "active_record/endoscope/version"
+require 'active_record/endoscope/version'
 
 module ActiveRecord
   module Endoscope
-    # Your code goes here...
+    def scope(name, scope_options = {})
+      super
+
+      instance_method_name = "#{name.to_s.sub(/^have_/, 'has_')}?"
+      class_eval <<-ENDOSCOPE, __FILE__, __LINE__ + 1
+        def #{instance_method_name}
+          self.class.#{name}.arel.to_ruby.call([self]).present?
+        end
+      ENDOSCOPE
+    end
   end
 end
