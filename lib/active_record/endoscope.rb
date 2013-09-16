7066fbf79b3058334fd0261d776caa167af59311
Toru KAWAMURA <tkawa@4bit.net>
2013-09-16 15:11:38 +0900

    Replace #arel with #build_arel
    For ignoring the default scope.

--- lib/active_record/endoscope.rb
+++ lib/active_record/endoscope.rb
@@ -8,7 +8,7 @@ module ActiveRecord
       instance_method_name = "#{name.to_s.sub(/^have_/, 'has_')}?"
 
       define_method(instance_method_name) do |*args|
-        self.class.send(name, *args).arel.to_ruby.call([self]).present?
+        self.class.send(name, *args).build_arel.to_ruby.call([self]).present?
       end
     end
   end
